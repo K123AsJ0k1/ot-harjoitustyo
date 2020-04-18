fe37abdf8fe058a434c4fd7653d641c3f8f35032
Niila Siilasjoki <niila.siilasjoki@gmail.com>
2020-04-18 10:28:18 +0300

    Projekti muutos

--- Fiktiotietokanta/src/main/java/Fiktiotietokanta/dao/usernameInterface.java
+++ Fiktiotietokanta/src/main/java/Fiktiotietokanta/dao/usernameInterface.java
@@ -5,24 +5,64 @@
  */
 package Fiktiotietokanta.dao;
 
-/**
+/** Käyttäjän ja käyttöliittymän rajapinta.
  *
- * @author niila
+ * 
  */
 public interface usernameInterface {
     
+    /** Luo tietokannan.
+    *
+    * 
+     * @return palauttaa true jos tietokanta on luotu ja false jos ei.
+     * @throws java.lang.Exception virhe.
+    */
     public boolean createUsernameDatabase() throws Exception;
     
+    /** Tarkastaa tietokannan olemassaolon.
+    *
+    * 
+     * @return palauttaa true jos tietokanta on luotu ja false jos ei.
+    */
     public boolean usernameDatabaseExists();
     
+    /** Lisää tietoa käyttäjä tietokantaan.
+    *
+    * 
+     * @param information annettu tieto.
+     * @return palauttaa true, jos tieto on lisätty ja false jos ei.
+    */
     public boolean addUserInformation(String information);
     
+    /** Tarkastaa halutun tiedon tietokannasta.
+    *
+    * 
+     * @param information annettu tieto.
+     * @return palauttaa true, jos tieto löytyy ja false jos ei.
+    */
     public boolean searchUserInformation(String information);
     
+    /** Hakee halutun käyttäjän id:n tietokannasta.
+    *
+    * 
+     * @param information annettu tieto.
+     * @return palauttaa tietokannan antaman id:n, jos käyttäjä löytyy ja palauttaa 0, jos ei.
+    */
     public Integer searchUsernameId(String information);
     
+    /** Poistaa halutun tiedon käyttäjä tietokannasta.
+    *
+    * 
+     * @param information annettu tieto.
+     * @return palauttaa true, jos poisto onnistui ja false jos ei.
+    */
     public boolean removeUserInformation(String information);
-    
+    /** Poistaa käyttäjä tietokannan kokonaan.
+    *
+    * 
+     * @return palauttaa true, jos poisto onnistui ja false jos ei.
+     * @throws java.lang.Exception virhe.
+    */
     public boolean removeUsernameDatabase() throws Exception;
     
 }
