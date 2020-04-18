6c6f75c2c85ff21032a5df5c9821c6a4c1565694
Niila Siilasjoki <niila.siilasjoki@gmail.com>
2020-04-18 10:47:59 +0300

    Projekti muutos

--- Fiktiotietokanta/src/main/java/Fiktiotietokanta/domain/nameDatabase.java
+++ Fiktiotietokanta/src/main/java/Fiktiotietokanta/domain/nameDatabase.java
@@ -15,15 +15,21 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
+/** Nimi tietokanta.
  *
- * @author niila
+ * 
  */
 public class nameDatabase implements databaseInterface {
     
     private Connection connection;
     private Boolean databaseExists;
     
+    
+    /** Nimi tietokanta konstruktori.
+    *
+    * 
+     * @throws java.lang.Exception virhe.
+    */
     public nameDatabase() throws Exception {
         this.connection = DriverManager.getConnection("jdbc:sqlite:namedatabase:connection");
         this.databaseExists = false;           
@@ -117,62 +123,85 @@ public class nameDatabase implements databaseInterface {
 
     @Override
     public boolean removeInformation(String givenName) {
-        try{
+        
+        try {
+            
             PreparedStatement command = connection.prepareStatement("DELETE FROM Names WHERE Name=?");
             command.setString(1, givenName);
             command.executeUpdate();
             command.close();
             return true;
-        }catch (SQLException k) {
+            
+        } catch (SQLException k) {
             
         }
+        
         return false;
+        
     }
 
     @Override
     public boolean removeDatabase() throws Exception {
+        
         try {
+            
             Statement command = connection.createStatement();
             command.execute("DROP TABLE Names;");
             command.close();
             databaseExists = false;
             return true;
+            
         } catch (SQLException k) {
             
         }
+        
         return false;
+        
     }
 
     @Override
     public String searchInformationTextIdentity(String givenNameId) {
+        
         int checkId = Integer.valueOf(givenNameId);
-        try{
+        
+        try {
+            
             PreparedStatement command = connection.prepareStatement("SELECT Name FROM Names WHERE id=?");
             command.setInt(1, checkId);
             ResultSet querySet = command.executeQuery();
             String givenTextIdentity = "null";
+            
             if (querySet.next()) {
                 givenTextIdentity = querySet.getString("Name");
             }
+            
             querySet.close();
             command.close();        
-            return givenTextIdentity;              
-        }catch (SQLException k) {
+            return givenTextIdentity;       
+            
+        } catch (SQLException k) {
             
         } 
+        
         return "null";
+        
     }
 
     @Override
     public List<String> showDatabaseAsAList() {
+        
         List<String> databaseAsAList = new ArrayList<>();
+        
         try {
+            
             PreparedStatement command = connection.prepareStatement("SELECT Name FROM Names;");
             ResultSet querySet = command.executeQuery();
+            
             while (querySet.next()) {
                 String givenClass = querySet.getString("Name");
                 databaseAsAList.add(givenClass);
             }
+            
             querySet.close();
             command.close();
             
@@ -181,11 +210,14 @@ public class nameDatabase implements databaseInterface {
         } catch (SQLException k) {
             
         } 
+        
         return null;
+        
     }
 
     @Override
     public List<String> showDatabaseAsARestrictedList(String information) {
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
