76e882d36e93519cc34c5a78d347d22a08d4d0f1
Niila Siilasjoki <niila.siilasjoki@gmail.com>
2020-04-18 10:09:48 +0300

    Projekti muutos

--- Fiktiotietokanta/src/main/java/Fiktiotietokanta/dao/databaseInterface.java
+++ Fiktiotietokanta/src/main/java/Fiktiotietokanta/dao/databaseInterface.java
@@ -8,21 +8,62 @@ package Fiktiotietokanta.dao;
 import java.sql.SQLException;
 import java.util.List;
 
-/**
- *
- * @author niila
+/** Tietokannat ja käyttöliittymän yhdistävä rajapinta.
+ * 
+ * 
  */
 public interface databaseInterface {
-    
+    /** Luo tietokannan.
+    * 
+    * 
+     * @return palauttaa true, jos tietokanta on luotu ja false jos ei.
+     * @throws java.lang.Exception ottaa vastaan virheet, jotka tapahtuvat.
+     * 
+    */
     public boolean createDatabase() throws Exception;
     
+    
+    /** Tarkastaa tietokannan olemassaolon.
+    * 
+    * 
+     * @return palauttaa true, jos tietokanta on olemassa ja false, jos ei.
+     * 
+    */  
     public boolean databaseExists();
     
+    
+    /** Lisää tietoa tietokantaan.
+    * 
+    * 
+     * @param information
+     * 
+     * @return palauttaa true, jos tiedon lisääminen onnistui ja false, jos ei. 
+     * 
+    */ 
     public boolean addInformation(String information);
     
+    
+    /** Tarkistaa halutun tiedon tietokannasta.
+    * 
+    * 
+     * @param information
+     * 
+     * @return palauttaa true, jos tietokannasta löytyy haettu tieto ja false jos ei.
+     * 
+    */ 
     public boolean searchInformation(String information);
     
+    
+    /** Hakee tietokannan antamaan id haetulle tiedolle.
+    * 
+    * 
+     * @param information
+     * 
+     * @return palauttaa tietokannan antaman id onnistuessa ja nollan, jos haettua tietoa ei löydy tietokannasta.
+     * 
+    */ 
     public Integer searchInfromationId(String information);
+    
     
     public String searchInformationTextIdentity(String information);
     
